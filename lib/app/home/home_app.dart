590687e71a0cc62a63c8f4997dc80d6844e02318
demola33 <akanmu.ademola2019@outlook.com>
2021-07-02 13:43:30 +0100

    Create the bottom navigation tab.

---
+++ lib/app/home/home_app.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:my_time_tracker/app/home/account/account_page.dart';
+import 'package:my_time_tracker/app/home/bottom_navigation.dart';
+import 'package:my_time_tracker/app/home/entries/entries_page.dart';
+import 'package:my_time_tracker/app/home/jobs/jobs_page.dart';
+import 'package:my_time_tracker/blocs/models/custom_user_model.dart';
+import 'package:my_time_tracker/blocs/models/user_profile.dart';
+import 'package:my_time_tracker/app/home/tab_item.dart';
+import 'package:my_time_tracker/services/database.dart';
+import 'package:provider/provider.dart';
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({Key key}) : super(key: key);
+
+  @override
+  HomeAppState createState() => HomeAppState();
+}
+
+class HomeAppState extends State<HomeApp>
+    with TickerProviderStateMixin<HomeApp> {
+  List<Key> _tabItemKeys;
+  List<AnimationController> _faders;
+  static int currentTab = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _faders = tabs.map<AnimationController>((TabItem tabItem) {
+      return AnimationController(
+          vsync: this, duration: Duration(milliseconds: 200));
+    }).toList();
+    _faders[currentTab].value = 1.0;
+    _tabItemKeys =
+        List<Key>.generate(tabs.length, (index) => GlobalKey()).toList();
+  }
+
+  @override
+  void dispose() {
+    for (AnimationController controller in _faders) controller.dispose();
+    super.dispose();
+  }
+
+  final List<TabItem> tabs = [
+    TabItem(
+      label: 'Jobs',
+      icon: Icons.work,
+      page: (_) => JobsPage(),
+      backgroundColor: Color.fromRGBO(0, 195, 111, 0.5),
+    ),
+    TabItem(
+      label: 'Entries',
+      icon: Icons.view_headline,
+      page: (context) => EntriesPage.create(context),
+      backgroundColor: Color.fromRGBO(0, 88, 72, 0.5),
+    ),
+    TabItem(
+      label: 'Account',
+      icon: Icons.account_circle,
+      page: (_) => AccountPage(),
+      backgroundColor: Color.fromRGBO(0, 144, 144, 0.5),
+    )
+  ];
+
+  HomeAppState() {
+    tabs.asMap().forEach((index, tabItem) {
+      tabItem.setIndex(index);
+    });
+  }
+
+  void _selectTab(int index) {
+    if (index == currentTab) {
+      tabs[index].key.currentState.popUntil((route) => route.isFirst);
+    } else {
+      setState(() => currentTab = index);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final database = Provider.of<Database>(context, listen: false);
+    return WillPopScope(
+      onWillPop: () async {
+        final isFirstRouteInCurrentTab =
+            !await tabs[currentTab].key.currentState.maybePop();
+        if (isFirstRouteInCurrentTab) {
+          if (currentTab != 0) {
+            _selectTab(0);
+            return false;
+          }
+        }
+        return isFirstRouteInCurrentTab;
+      },
+      child: StreamProvider<CustomUser>.value(
+        value: database.userProfileStream(),
+        child: Scaffold(
+          extendBody: true,
+          body: SafeArea(
+            top: false,
+            child: IndexedStack(
+              sizing: StackFit.expand,
+              index: currentTab,
+              children: tabs.map((tabItem) {
+                final Widget view = FadeTransition(
+                  opacity: _faders[tabItem.getIndex()]
+                      .drive(CurveTween(curve: Curves.fastOutSlowIn)),
+                  child: KeyedSubtree(
+                    key: _tabItemKeys[tabItem.getIndex()],
+                    child: tabItem.page,
+                  ),
+                );
+                if (tabItem.getIndex() == currentTab) {
+                  _faders[tabItem.getIndex()].forward();
+                  return view;
+                } else {
+                  _faders[tabItem.getIndex()].reverse();
+                  if (_faders[tabItem.getIndex()].isAnimating) {
+                    return IgnorePointer(child: view);
+                  }
+                  return Offstage(child: view);
+                }
+              }).toList(),
+            ),
+          ),
+          bottomNavigationBar: BottomNavigation(
+            onSelectTab: _selectTab,
+            currentIndex: currentTab,
+            tabs: tabs,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/app/home/tab_item.dart
+++ lib/app/home/tab_item.dart
@@ -1,33 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:my_time_tracker/app/home/home_app.dart';
 
-enum TabItem { jobs, entries, account }
-
-class TabItemData {
-  const TabItemData({
-    @required this.label,
-    @required this.icon,
-    this.backgroundColor,
-  });
-
+//enum TabItem { jobs, entries, account }
+class TabItem {
   final String label;
   final IconData icon;
   final Color backgroundColor;
+  final GlobalKey<NavigatorState> key = GlobalKey<NavigatorState>();
+  int _index = 0;
+  WidgetBuilder _page;
+  TabItem({
+    @required this.label,
+    @required this.icon,
+    @required this.backgroundColor,
+    @required WidgetBuilder page,
+  }) {
+    _page = page;
+  }
 
-  static const Map<TabItem, TabItemData> allTabs = {
-    TabItem.jobs: TabItemData(
-      label: 'Jobs',
-      icon: Icons.work,
-      //backgroundColor: Colors.white,
-    ),
-    TabItem.entries: TabItemData(
-      label: 'Entries',
-      icon: Icons.view_headline,
-      //backgroundColor: Colors.tealAccent,
-    ),
-    TabItem.account: TabItemData(
-      label: 'Account',
-      icon: Icons.account_circle,
-      //backgroundColor: Colors.lightBlueAccent,
-    ),
-  };
+  void setIndex(int i) {
+    _index = i;
+  }
+
+  int getIndex() => _index;
+
+  Widget get page {
+    return Visibility(
+      visible: _index == HomeAppState.currentTab,
+      maintainState: true,
+      child: Navigator(
+        key: key,
+        onGenerateRoute: (routeSettings) {
+          return MaterialPageRoute(
+            builder: _page,
+          );
+        },
+      ),
+    );
+  }
 }
+
+// const List<TabItem> allTabs = <TabItem>[
+//   TabItem('Jobs', Icons.work, Colors.deepOrangeAccent),
+//   TabItem('Entries', Icons.view_headline, Colors.tealAccent),
+//   TabItem('Account', Icons.account_circle, Colors.lightBlueAccent)
+// ];
+
+// class TabItemData {
+//   const TabItemData({
+//     @required this.label,
+//     @required this.icon,
+//     this.backgroundColor,
+//   });
+
+//   final String label;
+//   final IconData icon;
+//   final Color backgroundColor;
+
+//   static const Map<TabItem, TabItemData> allTabs = {
+//     TabItem.jobs: TabItemData(
+//       label: 'Jobs',
+//       icon: Icons.work,
+//       backgroundColor: Colors.white,
+//     ),
+//     TabItem.entries: TabItemData(
+//       label: 'Entries',
+//       icon: Icons.view_headline,
+//       //backgroundColor: Colors.tealAccent,
+//     ),
+//     TabItem.account: TabItemData(
+//       label: 'Account',
+//       icon: Icons.account_circle,
+//       //backgroundColor: Colors.lightBlueAccent,
+//     ),
+//   };
+// }
